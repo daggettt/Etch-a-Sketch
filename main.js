de1f6421b7e6258e31b0eafe38729ff2308047cd
daggettt <lorettadaggett.r@gmail.com>
2021-01-04 15:53:58 -0500

    Established grid, needs to be styled. Need to add dimension html btn.

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 // reference all elements
 
 const container = document.querySelector('#container');
-let rows = document.querySelectorAll('.gridRows');
-let columns = document.querySelectorAll('.gridColumns');
+let rows = document.getElementsByClassName('gridRows');
+let columns = document.getElementsByClassName('gridColumns');
+
+defaultGrid();
 
 // default 16 x 16 grid
-
 function defaultGrid () {
-    makeRows(16);
-    makeColumns(16);
+    makeRows(10);
+    makeColumns(10);
 }
 
 function makeRows(rowNum) {
     for (r = 0; r < rowNum; r++) {
         let row = document.createElement('div');
-        container.appendChild(row).className = 'gridRows';
+        container.appendChild(row).className = 'gridRows';  
     };
-};
+}
 
 function makeColumns(colNum) {
     for (i = 0; i < rows.length; i++) {
@@ -25,7 +26,4 @@ function makeColumns(colNum) {
             rows[j].appendChild(column).className = 'gridColumns';
         };
     };
-};
-
-defaultGrid();
-console.log(rows.length);
+}
